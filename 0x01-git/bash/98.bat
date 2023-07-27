7d0d0b9a29fe453870749935553d72d955e2c290
Isiaka Fuhad Olalekan <fuhadisiaka56@gmail.com>
2023-07-27 15:13:44 -0700

    My personal work

---
+++ 0x01-git/bash/98.bat
@@ -0,0 +1,11 @@
+On branch update_script
+Changes not staged for commit:
+  (use "git add <file>..." to update what will be committed)
+  (use "git restore <file>..." to discard changes in working directory)
+	modified:   alx.bat
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+	98.bat
+
+no changes added to commit (use "git add" and/or "git commit -a")
